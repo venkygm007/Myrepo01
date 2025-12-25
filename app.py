1425fdda4593a7359b69b4d1f02551ed2ca008f2
Venkygm007 <Venky.gm7@gmail.com>
2025-12-26 00:19:41 +0530

    Restore Flask app for Azure deployment

--- app.py
+++ app.py
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 776e018ab02f2f1402d9fdaa7665717815745a73
 from flask import Flask
-import os
-
 app = Flask(__name__)
 
-<<<<<<< HEAD
-
-@app.route("/")
-def home():
-    return "Hello Venky 👋 Your Flask App is running on Azure App Service 🚀"
-
-=======
-@app.route("/")
+@app.route('/')
 def home():
     return "Hello from Azure App Service!"
->>>>>>> 776e018ab02f2f1402d9fdaa7665717815745a73
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
